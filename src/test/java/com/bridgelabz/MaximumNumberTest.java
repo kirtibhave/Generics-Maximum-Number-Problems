e79fd91f5f3d7ec67cc1c9010a024b5f9d122aa6
kirtibhave <kirtibhave1000@gmail.com>
2020-04-02 22:37:11 +0530

    ADDED:Code for maximum number at second position return same number

--- src/test/java/com/bridgelabz/MaximumNumberTest.java
+++ src/test/java/com/bridgelabz/MaximumNumberTest.java
@@ -18,4 +18,9 @@ public class MaximumNumberTest {
         Assert.assertSame(86,maximumNumber.getMaximumNumberInteger(86,34,20));
     }
 
+    //1.2
+    @Test
+    public void givenMaximumIntegerNumber_WhenAtSecondPosition_ShouldReturnSameNumber(){
+        Assert.assertSame(86,maximumNumber.getMaximumNumberInteger(34,86,20));
+    }
 }
